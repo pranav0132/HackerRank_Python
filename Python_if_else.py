d87ef751c9931ee8c9896d2d613a286ae60ad3f8
Pranav Sharma <93445262+pranav0132@users.noreply.github.com>
2022-03-15 13:34:51 +0530

    Add files via upload

---
+++ Python_if_else.py
@@ -0,0 +1,9 @@
+n = int(input())
+if n%2!=0:
+    print("Weird")
+elif n%2==0 and n in range(2,6):
+    print("Not Weird")    
+elif n%2==0 and n  in range(6,21):
+    print("Weird")
+elif n>20 and n%2==0:
+    print("Not Weird")  
